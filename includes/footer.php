afb8b4ca4cf970bdbf3a37411837f8982e11c221
Hima Rani Mathews <68076769+HimaRaniMathews@users.noreply.github.com>
2020-12-18 11:39:31 +0530

    Update footer.php

--- includes/footer.php
+++ includes/footer.php
@@ -2,7 +2,7 @@
     <div class="container">
         <center>
             <br>
-            <p>Copyright &copy; PETS <i class="fas fa-paw"></i>  WORLD |  Contact Us: +91 90000 00000</p>	
+            <p>Copyright &copy; <?php echo DATE('Y') ;?>  PETS <i class="fas fa-paw"></i>  WORLD |  Contact Us: +91 90000 00000</p>	
         </center>
     </div>
 </footer>
